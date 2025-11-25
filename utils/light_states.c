227039a96cec96def70295e895a961dfe0332fa4
ville <ville.karppinen88@gmail.com>
2025-11-25 13:39:36 +0200

    Ex updated

--- utils/light_states.c
+++ utils/light_states.c
@@ -1,7 +1,7 @@
 #include "light_states.h"
 
 bool check_if_led_states_are_valid() {
-    for (int i = LED_L_ADDR; i < LED_R_ADDR; i+=2) {
+    for (int i = LED_L_ADDR; i <= LED_R_ADDR; i+=2) {
         led_state ls;
         ls.state = read_byte(i);
         ls.not_state = read_byte(i+1);
